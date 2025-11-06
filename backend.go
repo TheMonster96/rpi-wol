d905139fdbe757c90d0eac168821a9dbd399fcd3
TheMonster96 <francescopavone525@gmail.com>
2025-11-06 12:28:18 +0100

    Created a TCP listener and started creating the SSH handler

--- backend.go
+++ backend.go
@@ -3,10 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"net"
 	"net/http"
 	_ "net/url"
+	"time" 
+
+
+	//"golang.org/x/crypto/ssh"
 )
+
+var(
+	home_path = "/home/francesco/.ssh"
+)
+
+func loadAuthorizedKeys() (map[string]ssh.Publickey, error){
+
+	authorizedKeysBytes, err := os.ReadFile(home_path)
+
+	if err!= nil {
+		return nil, err
+	}
+
+	authorizedKeyMaps := make(map[string]bool)
+
+	for len(authorizedKeysBytes) > 0 {
+		pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
+		
+		if err != nil {
+			return nil, err		}
+
+		authorizedKeysMap[string(pubKey.Marshal())] = true
+		authorizedKeysBytes = rest
+	}
+
+	return authorizedKeyMaps, nil
+}
 
 func recvMACAddress(w http.ResponseWriter, req *http.Request){
 	_ = w
@@ -57,10 +89,91 @@ func recvMACAddress(w http.ResponseWriter, req *http.Request){
 
 func main(){
 
-	http.HandleFunc("/recvMACAddress", recvMACAddress)
+	authorizedKeyMaps, err:= loadAuthorizedKeys()
 
-	fmt.Printf("Starting http server")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-	//fmt.Printf("ciao")
+	if err != nil{
+		log.Fatal("Error: " err)
+	}
+
+	sshConfig := &ssh.ServerConfig{
+		
+		PublicKeyCallback: func(c ssh.ConnMetadata, pubkey ssh.PublicKey) (*ssh.Permissions, error) {
+			if authorizedKeyMaps[string(pubkey.Marshal()]{
+				return &ssh.Permissions{
+				Extensions: map[string]string{
+					"pubkey-fp": ssh.FingerprintSHA256(pubkey),
+				},
+			}, nil
+		} 
+		return nil, fmt.Error("Unknown public key for %q", c.User())
+		}
+	}
+
+	privateBytes, err := os.ReadFile(home_path + "id_ed25519")
+
+	if err != nil {
+		log.Fatal("Couldn't read/find private key: ", err)
+	}
+
+	privateKey, err := ssh.ParsePrivateKey(privateBytes)
+	
+	if err != nil {
+		log.Fatal("Couldn't parse private key: ", err)
+	}
+
+	config.AddHostKey(privateKey)
+
+	listener, err := net.Listen("tcp", "0.0.0.0:2222")
+
+	if err != nil {
+		log.Fatal("Failed to open TCP listener: ", err)  
+	}
+
+	fmt.Printf("Listening on 0.0.0.0:2222\n")
+
+	defer listener.Close()
+
+	for {
+		conn, err:= listener.Accept()
+
+		if err != nil {
+			fmt.Printf("Error while accepting incoming connection: %s", err)
+			continue
+		}
+
+		conn, chans, reqs, err:= ssh.NewServerConn(conn, config)
+		if err != nil {
+			fmt.Printf("Error while handshaking: %s\n", err)
+			continue
+		}
+
+		fmt.Printf("New connection request received from: %s at %v \n", conn.RemoteAddr(), time.Now())
+		fmt.Printf("Logged in with key %s", conn.Permissions.Extensions["pubkey-fp"])
+
+		go ssh.DiscardRequests(reqs)
+		buf:= make([]byte, 1024)
+		go func(c net.Conn)
+
+		go ssh.DiscardRequests(reqs)){
+		
+			defer conn.Close()
+		
+			for{
+			n, err:= conn.Read(buf)
+			if err != nil{
+				fmt.Printf("Error while reading, closing connection")
+				if error :=conn.Close(); error != nil{
+					fmt.Printf("Error while closing connection breh")
+				}
+				return
+			}
+
+			fmt.Print("Received bytes and length: %s, %d \n", string(buf[:n]), n)
+			
+			response := fmt.Sprintf("Received: %s", string(buf[:n]))
+			conn.Write([]byte(response))
+			}
+		}(conn)
 
 	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module rpi-wol/backend
+
+go 1.25.3
+
+require (
+	golang.org/x/crypto v0.43.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+)
