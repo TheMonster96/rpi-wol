ae51a4c72c5f93d034f11f7d8f75fedbccc59269
TheMonster96 <francescopavone525@gmail.com>
2025-11-07 12:22:57 +0100

    SSH server is working. Will have to implement TODOs written in the backend.go file

--- backend.go
+++ backend.go
@@ -1,31 +1,49 @@
 package main
 
+/*
+	TODO:
+	-- Make an sshServer struct which contains:
+		listener,
+		context,
+		sshConfig,
+		AuthorizedKeysMap,
+		...
+	-- Switch to a YAML file which contains: 
+		Authorized key
+		User name
+		MAC address
+		...
+	
+		
+
+
+
+*/
+
 import (
 	"fmt"
 	"log"
 	"os"
 	"net"
-	"net/http"
-	_ "net/url"
 	"time" 
 
 
-	//"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh"
 )
 
 var(
-	home_path = "/home/francesco/.ssh"
+	home_path = "/home/francesco/.ssh/"
 )
 
-func loadAuthorizedKeys() (map[string]ssh.Publickey, error){
+func loadAuthorizedKeys() (map[string]bool, error){
 
-	authorizedKeysBytes, err := os.ReadFile(home_path)
+	authorizedKeysBytes, err := os.ReadFile(home_path + "authorized_keys")
 
 	if err!= nil {
 		return nil, err
 	}
 
-	authorizedKeyMaps := make(map[string]bool)
+	authorizedKeysMap := make(map[string]bool)
 
 	for len(authorizedKeysBytes) > 0 {
 		pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
@@ -37,10 +55,10 @@ func loadAuthorizedKeys() (map[string]ssh.Publickey, error){
 		authorizedKeysBytes = rest
 	}
 
-	return authorizedKeyMaps, nil
+	return authorizedKeysMap, nil
 }
-
-func recvMACAddress(w http.ResponseWriter, req *http.Request){
+//TODO: Change this to an SSH Request instead of an HTTP Request
+/*func recvMACAddress(w http.ResponseWriter, req *http.Request){
 	_ = w
 		
 	query := req.URL.Query()
@@ -85,34 +103,34 @@ func recvMACAddress(w http.ResponseWriter, req *http.Request){
 	}
 
 
-}
+}*/
 
 func main(){
 
-	authorizedKeyMaps, err:= loadAuthorizedKeys()
+	authorizedKeysMap, err:= loadAuthorizedKeys()
 
 	if err != nil{
-		log.Fatal("Error: " err)
+		log.Fatal("Error: ", err)
 	}
 
 	sshConfig := &ssh.ServerConfig{
 		
 		PublicKeyCallback: func(c ssh.ConnMetadata, pubkey ssh.PublicKey) (*ssh.Permissions, error) {
-			if authorizedKeyMaps[string(pubkey.Marshal()]{
+			if authorizedKeysMap[string(pubkey.Marshal())] {
 				return &ssh.Permissions{
 				Extensions: map[string]string{
 					"pubkey-fp": ssh.FingerprintSHA256(pubkey),
 				},
 			}, nil
 		} 
-		return nil, fmt.Error("Unknown public key for %q", c.User())
-		}
+		return nil, fmt.Errorf("Unknown public key for %q", c.User())
+		},
 	}
 
 	privateBytes, err := os.ReadFile(home_path + "id_ed25519")
 
 	if err != nil {
-		log.Fatal("Couldn't read/find private key: ", err)
+		log.Fatal("Couldn't read/find private key:  \n use 'ssh-keygen -t rsa' to generate a key pair", err)
 	}
 
 	privateKey, err := ssh.ParsePrivateKey(privateBytes)
@@ -121,7 +139,7 @@ func main(){
 		log.Fatal("Couldn't parse private key: ", err)
 	}
 
-	config.AddHostKey(privateKey)
+	sshConfig.AddHostKey(privateKey)
 
 	listener, err := net.Listen("tcp", "0.0.0.0:2222")
 
@@ -141,39 +159,52 @@ func main(){
 			continue
 		}
 
-		conn, chans, reqs, err:= ssh.NewServerConn(conn, config)
+		sshConn, chans, reqs, err:= ssh.NewServerConn(conn, sshConfig)
 		if err != nil {
 			fmt.Printf("Error while handshaking: %s\n", err)
 			continue
 		}
 
 		fmt.Printf("New connection request received from: %s at %v \n", conn.RemoteAddr(), time.Now())
-		fmt.Printf("Logged in with key %s", conn.Permissions.Extensions["pubkey-fp"])
+		fmt.Printf("Logged in with key %s\n", sshConn.Permissions.Extensions["pubkey-fp"])
 
 		go ssh.DiscardRequests(reqs)
-		buf:= make([]byte, 1024)
-		go func(c net.Conn)
 
-		go ssh.DiscardRequests(reqs)){
-		
-			defer conn.Close()
-		
-			for{
-			n, err:= conn.Read(buf)
-			if err != nil{
-				fmt.Printf("Error while reading, closing connection")
-				if error :=conn.Close(); error != nil{
-					fmt.Printf("Error while closing connection breh")
-				}
-				return
-			}
-
-			fmt.Print("Received bytes and length: %s, %d \n", string(buf[:n]), n)
+		for newChannel:= range chans{
 			
-			response := fmt.Sprintf("Received: %s", string(buf[:n]))
-			conn.Write([]byte(response))
+			if newChannel.ChannelType() != "session" {
+				newChannel.Reject(ssh.UnknownChannelType, "Type of channel is unknown")
+				continue
 			}
-		}(conn)
+			channel, _ , err := newChannel.Accept()
+			if err != nil {
+				log.Fatal("Could not accept channel, %v\n", err)
+			}
+			go func(){
+		
+				//defer channel.Close()
+			
+				for{
+				/*n, err:= channel.Read(buf)
+				if err != nil{
+					fmt.Printf("Error while reading, closing channel connection")
+					if error :=channel.Close(); error != nil{
+						fmt.Printf("Error while closing channel connection breh")
+					}
+					return
+				}*/
 
+				//fmt.Print("Received bytes and length: %s, %d \n", string(buf[:n]), n)
+				
+				response := fmt.Sprintf("Buongiorno\n")
+				channel.Write([]byte(response))
+				channel.Close()	
+				}
+			}()
+
+
+		}
+		//buf:= make([]byte, 1024)
+		
 	}
 }
